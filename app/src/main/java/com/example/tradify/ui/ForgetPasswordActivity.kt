0df97d2a7cd0259672683f1a886719a75d9df0dc
rizaadi <rizaadi890@gmail.com>
2022-06-05 14:47:40 +0700

    UI Signin,singup,forget password

---
+++ app/src/main/java/com/example/tradify/ui/ForgetPasswordActivity.kt
@@ -0,0 +1,12 @@
+package com.example.tradify.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.tradify.R
+
+class ForgetPasswordActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_forget_password)
+    }
+}
---
+++ app/src/main/java/com/example/tradify/ui/signin/SigninActivity.kt
@@ -0,0 +1,12 @@
+package com.example.tradify.ui.signin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.tradify.R
+
+class SigninActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signin)
+    }
+}
